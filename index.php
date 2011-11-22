24f5b887174bd81abfa6cd72bb60df8bc882f9e9
Eric Teubert <ericteubert@googlemail.com>
2011-11-22 19:20:26 +0100

    add context

--- index.php
+++ index.php
@@ -1,4 +1,10 @@
-<strong>Hey!</strong> Yes, you!
-<?php
-
-echo "Hello World!";
+<html>
+	<head></head>
+	<body>
+		
+		<strong>Hey!</strong>
+		Yes, you!
+		<?php echo "Hello World!"; ?>
+		
+	</body>
+</html>
